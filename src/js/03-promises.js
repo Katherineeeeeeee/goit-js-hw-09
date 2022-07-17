1e56f9200254920a0c6627ca9d6bbde8d94da8cc
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-17 17:14:37 +0300

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const submitEl = event => {
+  event.preventDefolt();
+
+  const delayEl = Number(delay.value);
+  const stepEl = Number(step.value);
+  const amountEl = Number(amount.value);
+
+  for (let i = 0; i < amountEl; i += 1) {
+    createPromise(i, delayEl)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  event.currentTarget.reset();
+};
+
+
+formEl.addEventListener('submit', submitEl);
+
